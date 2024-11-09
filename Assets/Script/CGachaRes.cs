828c2eacfc7723f330658d0f3ba2a3440c746817
eklkori-admin <8031edokorilove@gmail.com>
2024-11-09 13:40:12 +0900

    ガチャ結果画面作成完了　キャラ詳細ポップアップ作成中

--- Assets/Script/CGachaRes.cs
+++ Assets/Script/CGachaRes.cs
@@ -9,6 +9,8 @@ public class CGachaRes : MonoBehaviour
     [SerializeField] GameObject tapT;
     [SerializeField] GameObject tanpatsu;
     [SerializeField] GameObject jyuren;
+    [SerializeField] GameObject newIcon;
+    [SerializeField] GameObject[] newIcons;
     [SerializeField] Image kyara;
     [SerializeField] Image[] kyaras;
     [SerializeField] Sprite[] kyaraImages;
@@ -34,6 +36,12 @@ public class CGachaRes : MonoBehaviour
 
         //キャラ画像差し替え
         kyara.sprite = kyaraImages[AkagonohateData.gacha10[0]];
+
+        //未獲得キャラの場合、new!を表示
+        newIcon.SetActive(false);
+        if (AkagonohateData.gachaNotNew[0] == 0) {
+            newIcon.SetActive(true);
+        }
     }
 
     /// <summary>
@@ -45,7 +53,17 @@ public class CGachaRes : MonoBehaviour
 
         //キャラ画像差し替え
         for (int i = 0; i < 10; i++) {
-            kyara.sprite = kyaraImages[AkagonohateData.gacha10[i]];
+            kyaras[i].sprite = kyaraImages[AkagonohateData.gacha10[i]];
+        }
+
+        //未獲得キャラの場合、new!を表示
+        for (int i = 0; i < 10; i++)
+        {
+            newIcons[i].SetActive(false);
+            if (AkagonohateData.gachaNotNew[i] == 0)
+            {
+                newIcons[i].SetActive(true);
+            }
         }
     }
 
@@ -53,7 +71,6 @@ public class CGachaRes : MonoBehaviour
     {
         //「-TAP-」を点滅させる処理
         Color colorTap = tapT.GetComponent<Image>().color;
-        //tapT.GetComponent<Image>().color = colorTap;
         if (tenmetsuFlg == 0)
         {
             colorTap.a -= 0.02f;
